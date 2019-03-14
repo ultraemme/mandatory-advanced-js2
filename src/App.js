e1041fb55784f9c1c294b6a0d024f0c4b013d11e
Emil Larsson <emil.larsson@utb.ecutbildning.se>
2019-03-14 11:31:35 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+//https://reacttraining.com/react-router/web/guides/quick-start
+import Add from './components/Add';
+import Edit from './components/Edit';
+import Details from './components/Details';
+import Main from './components/Main';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount () {
+    axios.get('http://ec2-13-53-132-57.eu-north-1.compute.amazonaws.com:3000/movies')
+      .then(res => {
+        this.setState({res: res.data});
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/main/">Main</Link></li>
+              <li><Link to="/add/">Add movie</Link></li>
+              <li><Link to="/edit/">Edit movie</Link></li>
+              <li><Link to="/details/">Details</Link></li>
+            </ul>
+          </nav>
+          <Route path="/main/" component={() => <Main data={this.state.res}/>}/>
+          <Route path="/add/" component={Add}/>
+          <Route path="/edit/" component={Edit}/>
+          <Route path="/details/" component={Details}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Edit.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Edit extends Component {
+  render() {
+    return (
+      <h1>Edit</h1>
+    );
+  }
+}
+
+export default Edit;
